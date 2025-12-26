030eef679104859b3f071a001cf95ec6779f1462
PALLAB BANERJEE <pallabbanerjee074@gmail.com>
2025-12-26 23:19:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,16 @@ const cors = require("cors");
 
 // IMPORTANT: handle preflight
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
-  credentials: true,
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://ecoscan-ai.netlify.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
 
 
 const PORT = process.env.PORT || 5000;
