ec797624759e7044d4a0531e7b72bbaad6539943
PALLAB BANERJEE <pallabbanerjee074@gmail.com>
2025-12-28 11:17:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.options("*", cors());
 
 
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const upload = multer();
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
